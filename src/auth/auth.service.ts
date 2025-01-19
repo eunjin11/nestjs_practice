e41bcf02854d288cda1173bd66c8b66ecbe3a6d5
Eunjin Cho <whdmswls9207@gmail.com>
2025-01-19 14:29:46 +0900

    feat : add user signup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
+import { AuthCredentialDto } from './dto/auth-credential';
 
 @Injectable()
 export class AuthService {
@@ -9,4 +10,15 @@ export class AuthService {
     @InjectRepository(User)
     private userRepository: Repository<User>,
   ) {}
+
+  async createUser(authCredentialDto: AuthCredentialDto): Promise<void> {
+    const { username, password } = authCredentialDto;
+    const user = this.userRepository.create({ username, password });
+    await this.userRepository.save(user);
+  }
+
+  async signUp(authCredentialDto: AuthCredentialDto): Promise<void> {
+    await this.createUser(authCredentialDto);
+    return;
+  }
 }
